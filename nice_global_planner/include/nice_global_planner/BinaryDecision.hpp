d47fe74940965eb7954ef25ed743fca966dc793e
Farhan Rahman <farhanrahman26@gmail.com>
2013-04-05 16:49:13 +0100

    Moved code around in different files

---
+++ nice_global_planner/include/nice_global_planner/BinaryDecision.hpp
@@ -0,0 +1,10 @@
+#ifndef BINARY_DECISION_HPP
+#define BINARY_DECISION_HPP
+
+namespace nice_global_planner{
+	typedef enum BinaryDecision{
+		LEFT = 0, RIGHT = 1
+	} BinaryDecision;	
+}
+
+#endif
--- nice_global_planner/include/nice_global_planner/KDTree.hpp
+++ nice_global_planner/include/nice_global_planner/KDTree.hpp
@@ -1,135 +1,17 @@
 #ifndef KD_TREE_HPP
 #define KD_TREE_HPP
+#include <iostream>
 #include <vector>
 #include <assert.h>
 #include <map>
 #include <limits.h>
+#include <math.h>
+
+#include "BinaryDecision.hpp"
+#include "KDNode.hpp"
+
 
 namespace nice_global_planner{
-
-typedef enum BinaryDecision{
-	LEFT = 0, RIGHT = 1
-} BinaryDecision;
-
-template<typename T>
-class KDNode{
-public:
-	KDNode (const std::vector<T>& inputData, unsigned decisionDim)
-	: decisionDim(decisionDim)
-	{
-		data.resize(inputData.size());
-		for(unsigned i = 0; i < inputData.size(); ++i){
-			this->data[i] = inputData[i];
-		}
-		this->commonInitialiser();
-	}
-
-	KDNode(unsigned dim, unsigned decisionDim)
-	: decisionDim(decisionDim)
-	{
-		data.resize(dim);
-		this->commonInitialiser();
-	}
-
-	BinaryDecision getNextDirection(const std::vector<T>& data){
-		if(this->data[decisionDim] < data[decisionDim]){
-			return RIGHT;
-		} else {
-			return LEFT;
-		}
-	}
-
-	BinaryDecision getNextDirection(const KDNode<T> *node){
-		if(this->data[decisionDim] < (*node).data[decisionDim]){
-			return RIGHT;
-		} else {
-			return LEFT;
-		}
-	}
-
-	KDNode * getNextStorageNode(const std::vector<T>& data){
-		BinaryDecision binaryDecision = this->getNextDirection(data);
-		if(binaryDecision == LEFT){
-			return left;
-		} else {
-			return right;
-		}
-	}
-
-	unsigned getNextDecisionDim(unsigned dim){
-		return (this->decisionDim + 1)%dim;
-	}
-
-	T getSplittingValue(void){
-		return (*this).data[decisionDim];
-	}
-
-	unsigned getSplittingAxis(void){
-		return this->decisionDim;
-	}
-
-
-	bool equals(const KDNode<T> *node){
-		bool equal = false;
-
-		if((*node).data.size() != (*this).data.size()){
-			return equal;
-		}
-
-		for(unsigned i = 0; i < (*this).data.size(); ++i){
-			if((*this).data[i] != (*node).data[i]){
-				return equal;
-			}
-		}
-		
-		equal = true;
-		return equal;
-	}
-
-	double distance(const KDNode<T> *node){
-		return sqrt(this->sqDistance(node));
-	}
-
-	double sqDistance(const KDNode<T> *node){
-		double sqDistance = 0.0;
-		for(unsigned i = 0; i < data.size(); ++i){
-			double diff = this->data[i] - (*node).data[i];
-			sqDistance += diff*diff;
-		}
-		return sqDistance;
-	}
-
-	void printNode(void){
-		std::cout << "[";
-		for(unsigned i = 0; i < data.size(); ++i){
-			if(i == data.size() - 1){
-				std::cout << "data["<< i << "]: " << data[i];
-			} else {
-				std::cout << "data[" << i << "]: " << data[i] << ",";
-			}
-		}
-		std::cout << "]" << std::endl;
-	}
-
-	static KDNode<T> createTempNode(const std::vector<T>& data){
-		return KDNode<T> (data, 0);
-	}
-
-
-	std::vector<T> data;
-	KDNode *left, *right;
-	KDNode *parent;
-
-private:
-	unsigned decisionDim;
-
-	void commonInitialiser(void){
-		left = NULL;
-		right = NULL;
-		parent = NULL;
-	}
-};
-
 
 template<typename T>
 class KDTree{
