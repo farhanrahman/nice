513c5e70692a84adaf00f7ba5ab8f812202dbb55
Farhan Rahman <farhanrahman26@gmail.com>
2013-06-02 18:50:45 +0100

    Safe commit

--- nice_core/include/nice_core/NiceCore.hpp
+++ nice_core/include/nice_core/NiceCore.hpp
@@ -1,221 +1,71 @@
-#ifndef NICE_CORE_HPP
-#define NICE_CORE_HPP
+#ifndef NICE_CORE
+#define NICE_CORE
 #include <ros/ros.h>
-#include <move_base_msgs/MoveBaseActionGoal.h>
-#include <move_base_msgs/MoveBaseActionFeedback.h>
-#include <actionlib_msgs/GoalStatusArray.h>
-
-#include <nav_msgs/GetPlan.h>
-
-#include <std_srvs/Empty.h>
-#include <std_srvs/Empty.h>
-
-#include <actionlib_msgs/GoalID.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <move_base_msgs/MoveBaseActionResult.h>
+#include <actionlib/client/simple_action_client.h>
 
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PointStamped.h>
 
 #include <boost/thread/mutex.hpp>
-#include <boost/thread.hpp>
-
-#include <vector>
-
-#include <tf/transform_broadcaster.h>
-#include <tf/transform_listener.h>
-#include <tf/tfMessage.h>
-#include <geometry_msgs/TransformStamped.h>
-
-#include <costmap_update/tfMessageParser.hpp>
-#include <costmap_update/ParserMessage.hpp>
-
-#include <nice_detector/Detection.h>
-#include <nice_detector/DetectionList.h>
-
-#include <queue>
-#include <nice_core/DetectionMessage.hpp>
-
-#include <boost/shared_ptr.hpp>
+#include <actionlib/action_definition.h>
 
 namespace nice_core
 {
 
 typedef enum CoreMode{
 	IDLE = 0,
-	FOLLOWING = 1,
-	PLANNING = 2
+	PLANNING = 1,
+	FOLLOWING
 } CoreMode;
-
-typedef boost::shared_ptr<DetectionMessage> DetectionMsgPtr;
 
 class NiceCore
 {
 public:
 	NiceCore(
 		ros::NodeHandle& n,
-		unsigned glbs = 1000,
-		unsigned gpbs = 1000,
-		unsigned gcbs = 1000,
-		unsigned rplbs = 1000,
-		unsigned rate = 10
+		double rate = 10
 	);
-	~NiceCore();
+	~NiceCore(){}
 
-	/**
-	 *@brief Main segment of the code that will get
-	 * executed as part of a ros node
-	 */
 	void nodeLoop(void);
 
 private:
-	/**
-	 *@brief Callback function for a new goal published from an
-	 *  external node. Will forward the goal if necessary to
-	 *  move_base node.
-	 *@param msg Goal point being sent on the topic
-	 */
-	void GoalListenerCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
-
-	/**
-	 * @brief Thread safe method to retrieve the last saved goal
-	 * @return The last updated goal position assigned to the robot
-	 */
-	geometry_msgs::PoseStamped getGoal(void);
-
-	/**
-	 * @brief Thread safe method to update the goal
-	 * @param g New goal position to be set
-	 */
-	void setGoal(const geometry_msgs::PoseStamped::ConstPtr& msg);
-	/**
-	 * @brief Updates the feedback about the position of the robot base
-	 *  by listening to the robot base information published on the feedback
-	 *  topic by move_base
-	 * @param msg Feedback message published by move_base
-	 */
-	void robotPositionListenerCallBack(const move_base_msgs::MoveBaseActionFeedback::ConstPtr& msg);
+	double rate;
 
 
-	/**
-	 * @brief Function that sends a goal position to the move_base
-	 * @param goal New goal position to be sent to the move_base module
-	 */
-	void sendGoal(const geometry_msgs::PoseStamped::ConstPtr& goal);
+	void mainGoalListenerCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+	void setMainGoal(const geometry_msgs::PoseStamped& poseStamped);
+	move_base_msgs::MoveBaseGoal getMainGoal(void);
 
-	/**
-	 * @brief Function that sends a goal position to the move_base
-	 * @param goal New goal position of type geometry_msgs::PoseStamped
-	 */
-	void sendGoal(const geometry_msgs::PoseStamped& goal);
-
-	/**
-	 * @brief Cancels a goal with a particular ID
-	 * @param goalID ID of the goal to cancel
-	 **/
-	void cancelGoal(const actionlib_msgs::GoalID goalId);
-
-	/**
-	 * @brief Returns the most upto date value of the robot's base
-	 *  as reported by the move_base module
-	 * @return The most upto date robot position
-	 */
-	geometry_msgs::PoseStamped getRobotPosition(void);
+	ros::NodeHandle* nodeHandlePtr;
 
 
-	/**
-	 * @brief Callback function for tf messages published on the
-	 *  openni_tracker topic
-	 * @param msg An array of TransformStamped messages
-	 **/
-	void tfCallback(const tf::tfMessage::ConstPtr& msg);
+	typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+	MoveBaseClient* ac;
+	void doneCallback(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result);
 
-	/**
-	 * @brief Thread safe getter function
-	 * @return Returns a copy of the Core Mode
-	 **/
-	CoreMode getCoreMode(void);
+	boost::mutex mainGoalMutex;
+	move_base_msgs::MoveBaseGoal mainGoal;
+	ros::Subscriber mainGoalListener;
 
-	/**
-	 * @brief Thread safe method to update the core module mode
-	 * @param c New mode to be set
-	 **/
-	void setCoreMode(CoreMode c);
-
-	/**
-	 * @brief Thread safe method to update the following agent number
-	 * @param fa The agent number to be set that the base_link should follow
-	 **/
-	void setFollowingAgent(int fa);	
-
-	/**
-	 * @brief Calculates the squared distance in the Euclidian space
-	 * @return Returns the squared Euclidian distance
-	 */
-	double getDistanceSq(const geometry_msgs::Transform& transform);
-
-	/**
-	 * @brief Checks whether a particular agent is facing the goal
-	 * @param transform Transform of the agent
-	 * @return True if the agent with transform is facing the goal
-	 */
-	bool facingGoal(const geometry_msgs::Transform& transform);
-
-
-	/**
-	 * @brief Method for retreiving the following agent number
-	 * @return Returns the following agent number in a thread safe method
-	 **/
-	int getFollowingAgent(void);
-
-	/**/
-	void detectionCallback(const nice_detector::DetectionListConstPtr& detections);
-
-
-	geometry_msgs::PointStamped getWorldPoint(geometry_msgs::PointStampedPtr& p, const char *frame);
-
-
-	ros::Subscriber goalListener;
-	ros::Publisher coreGoalPublisher;
-	ros::Publisher goalCanceller;
-
-	ros::Subscriber robotPositionListener;
-
-	ros::ServiceClient planService;
-	ros::ServiceClient clearUnknownService;
-	ros::ServiceClient clearCostmapService;
-
-	ros::NodeHandle* nhPtr;
-
-	unsigned goalListenerBufferSize;
-	unsigned goalPublisherBufferSize;
-	unsigned goalCancellerBufferSize;
-	unsigned robotPositionListenerBufferSize;
-	unsigned rate;
-
-	boost::mutex goalLock;
-	geometry_msgs::PoseStamped goal;
-
-	move_base_msgs::MoveBaseActionFeedback feedback;
-
-	bool newGoalPublished;
-	boost::mutex newGoalLock;
-
-	ros::Subscriber tfListener;
 
 	boost::mutex cmMutex;
 	CoreMode coreMode;
 
-	tf::TransformListener tfl;
+	void setCoreMode(CoreMode cm){
+		boost::mutex::scoped_lock(cmMutex);
+		this->coreMode = cm;
+	}
 
-	int followingAgent;
-
-	ros::Publisher pub;
-
-	ros::Subscriber detection;
-
-	boost::mutex dqMutex;
-	std::queue<DetectionMessage> detectionQueue;
+	CoreMode getCoreMode(void){
+		boost::mutex::scoped_lock(cmMutex);
+		CoreMode ret = this->coreMode;
+		return ret;
+	}
 
 };
-
 }
 
 #endif
--- nice_core/src/NiceCore.cpp
+++ nice_core/src/NiceCore.cpp
@@ -1,439 +1,78 @@
 #include <nice_core/NiceCore.hpp>
-#include <limits>
-#include <geometry_msgs/Twist.h>
-#include <sstream>
-#include <string>
 
 namespace nice_core
 {
-
 NiceCore::NiceCore(
-		ros::NodeHandle& nh,
-		unsigned glbs,
-		unsigned gpbs,
-		unsigned gcbs,
-		unsigned rplbs,
-		unsigned rate
+	ros::NodeHandle& n,
+	double rate
 ) : 
-	nhPtr(&nh),
-	goalListenerBufferSize(glbs),
-	goalPublisherBufferSize(gpbs),
-	goalCancellerBufferSize(gcbs),
-	robotPositionListenerBufferSize(rplbs),
+	nodeHandlePtr(&n),
 	rate(rate)
 {
-	/*Initialise the subscribers and publishers*/
-	goalListener = nh.subscribe(
-						"move_base_simple/goal", 
-						goalListenerBufferSize, 
-						&NiceCore::GoalListenerCallback, 
+	ac = new MoveBaseClient("move_base", true);
+
+	mainGoalListener = n.subscribe(
+						"nice_core/main_goal",
+						10,
+						&NiceCore::mainGoalListenerCallback,
 						this
-					);
-
-	detection = nh.subscribe(
-						"detections",
-						1,
-						&NiceCore::detectionCallback,
-						this
-					);
-
-	coreGoalPublisher = nh.advertise<geometry_msgs::PoseStamped>(
-						"move_base_simple/filtered_goal",
-						goalPublisherBufferSize
-					);
-
-	
-	goalCanceller = nh.advertise<actionlib_msgs::GoalID>(
-						"actionlib_msgs/GoalID",
-						goalCancellerBufferSize
-					);
-
-
-	robotPositionListener = nh.subscribe(
-						"move_base/feedback",
-						robotPositionListenerBufferSize,
-						&NiceCore::robotPositionListenerCallBack,
-						this
-					);
-
-	// tfListener = nh.subscribe(
-	// 					"tf",
-	// 					1000,
-	// 					&NiceCore::tfCallback,
-	// 					this
-	// 				);
-
-	pub = nh.advertise<geometry_msgs::Twist>(
-                		"cmd_vel",
-                		10
-                	);
-
-	/*Initialise the clients*/
-	planService = nh.serviceClient<nav_msgs::GetPlan> ("make_plan");
-	clearUnknownService = nh.serviceClient<std_srvs::Empty>("clear_unknown_space");
-	clearCostmapService = nh.serviceClient<std_srvs::Empty>("clear_costmaps");
+				);
 
 	coreMode = IDLE;
-	followingAgent = -1;
+}	
+	
+void NiceCore::setMainGoal(const geometry_msgs::PoseStamped& poseStamped){
+	boost::mutex::scoped_lock(mainGoalMutex);
+	mainGoal.target_pose.header.seq = poseStamped.header.seq;
+	mainGoal.target_pose.header.stamp = poseStamped.header.stamp;
+	mainGoal.target_pose.header.frame_id = poseStamped.header.frame_id;
 
+	mainGoal.target_pose.pose.position.x = poseStamped.pose.position.x;
+	mainGoal.target_pose.pose.position.y = poseStamped.pose.position.y;
+	mainGoal.target_pose.pose.position.z = poseStamped.pose.position.z;
 
-
-
+	mainGoal.target_pose.pose.orientation.x = poseStamped.pose.orientation.x;
+	mainGoal.target_pose.pose.orientation.y = poseStamped.pose.orientation.y;
+	mainGoal.target_pose.pose.orientation.z = poseStamped.pose.orientation.z;
+	mainGoal.target_pose.pose.orientation.w = poseStamped.pose.orientation.w;
 }
 
-NiceCore::~NiceCore(void){
+move_base_msgs::MoveBaseGoal NiceCore::getMainGoal(void){
+	boost::mutex::scoped_lock(mainGoalMutex);
+	move_base_msgs::MoveBaseGoal ret = this->mainGoal;
+	return ret;
+}
+
+void NiceCore::doneCallback(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result)
+{
+	ROS_INFO("DONE");
+	this->setCoreMode(IDLE);
+}
+
+void NiceCore::mainGoalListenerCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	this->setMainGoal(*msg);
+	this->setCoreMode(PLANNING);
+
+	ROS_INFO("Sending goal");
+	boost::function<void (const actionlib::SimpleClientGoalState&, const move_base_msgs::MoveBaseResultConstPtr&) > f;
+
+	f = boost::bind(&NiceCore::doneCallback, this, _1, _2);
+
+	ac->sendGoal(this->getMainGoal(), f);
+
+	// ac->waitForResult();
+	// this->setCoreMode(IDLE);
 }
 
 
 void NiceCore::nodeLoop(void){
-	ros::Rate r((*this).rate);
-	ros::NodeHandle n;
+	ros::Rate r(this->rate);
 
-    const double frequency = this->rate;
-    const double timeToUser = 6; //Time for robot to reach to user's position
-    const double minDistance = 0.4; //30 cm
-    const double minAngle = 0.09; //~5deg
-    const double maxLinearSpeed = 0.3;// in metres per second
-    const double maxAngularSpeed = 0.52; //~30 deg per second
-
-    // Maximum time for transform to be available
-    const double timeout = 0.1;
-
-    const std::string destFrame = "/base_link";
-	// const std::string destFrame = "/camera_depth_frame";
-	const std::string originFrame = "/odom";
-	while(n.ok()){
-
-		CoreMode cmCopy = this->getCoreMode();
-		DetectionMessage latest;
-		DetectionMessage oldest;		
-
-		if(coreMode != IDLE){
-			std::queue<DetectionMessage> dqCopy;
-			{
-				boost::mutex::scoped_lock(dqMutex);
-				for(unsigned i = 0; i < detectionQueue.size(); ++i){
-					DetectionMessage dmsg = detectionQueue.front();
-					dqCopy.push(dmsg);
-					detectionQueue.pop();
-				}
-			}
-
-			if(dqCopy.size() == 0){
-				// ROS_INFO("dqCopy size is 0. Cannot continue");
-				this->setCoreMode(PLANNING);
-				if(cmCopy == FOLLOWING){
-					this->sendGoal((*this).getGoal());
-					cmCopy = PLANNING;
-				}
-			} else {
-				latest = dqCopy.back();
-				oldest = dqCopy.front();			
-				double dt = latest.distance - oldest.distance;
-
-				// if(dt >= 0.0){
-					this->setCoreMode(FOLLOWING);
-					geometry_msgs::PoseStamped goalCopy = this->getGoal();
-					actionlib_msgs::GoalID goalId;
-					goalId.stamp = goalCopy.header.stamp;
-					goalId.id = goalCopy.header.frame_id;
-					this->cancelGoal(goalId);
-				// }
-				// else {
-				// 	this->setCoreMode(PLANNING);
-				// 	if(cmCopy == FOLLOWING){
-				// 		this->sendGoal((*this).getGoal());
-				// 		cmCopy = PLANNING;
-				// 	}
-				// }
-			}
-		}
-
-
-
-		if (cmCopy == FOLLOWING){
-			ROS_INFO("FOLLOWING");
-			// std::stringstream ss;
-			// ss << "torso" << std::endl;
-			// ss >> originFrame;
-
-	        // bool okay = tfl.waitForTransform(
-	        //             destFrame,
-	        //             originFrame,
-	        //             ros::Time::now() - ros::Duration(timeout), //Need recent tf
-	        //             ros::Duration(timeout)
-	        //             );
-
-	        double linearSpeed = 0.0;
-        	double angularSpeed = 0.0;
-
-	        // if(okay){
-	            geometry_msgs::PointStamped input;
-	            input.header.frame_id = originFrame;
-	            input.header.stamp = ros::Time(0);
-
-				geometry_msgs::PointStamped pointToFollow = latest.point;
-
-	            input.point.x = pointToFollow.point.x;
-	            input.point.y = pointToFollow.point.y;
-	            input.point.z = pointToFollow.point.z;
-
-	            geometry_msgs::PointStamped output;
-
-	            // TODO: Do you understand what this function is doing?
-	            tfl.transformPoint( destFrame, input, output );
-
-	            output.point.x = pointToFollow.point.x;
-	            output.point.y = pointToFollow.point.y;
-	            output.point.z = pointToFollow.point.z;
-
-	            // README: Now we use the transformed point to calculate the
-	            // required translational and rotational speeds.
-
-	            // TODO: Compute the euclidian distance between the torso and
-	            // the robot.
-	            // HINT: To make things simpler, we project the point to the plane
-	            // of the robot (in other words, we ignore the z value)
-	            double distance = std::sqrt(
-	                        output.point.x * output.point.x +
-	                        output.point.y * output.point.y
-	                        );
-
-	            // TODO: Do you understand what this line of code is doing?
-	            // HINT: http://www.cplusplus.com/reference/clibrary/cmath/atan2/
-	            double angle = std::atan2(output.point.y, output.point.x);
-
-	            // README: The two blocks of code make sure we don't go faster
-	            // than the limits specified at the beginning of the file.
-	            // WARNING: These blocks are safety-critical, please do not modify.
-	            if ( std::abs(angle) > minAngle )
-	            {
-	                angularSpeed = std::abs( angle*frequency / timeToUser );
-
-	                if (angularSpeed > maxAngularSpeed)
-	                    angularSpeed = maxAngularSpeed;
-
-
-	                if ( angle < 0 )
-	                    angularSpeed = -angularSpeed;
-
-	            }
-	            if ( distance > minDistance )
-	            {
-	                linearSpeed =  distance * frequency /timeToUser ;
-
-	                if (linearSpeed > maxLinearSpeed)
-	                    linearSpeed = maxLinearSpeed;
-
-	            }	        	
-	        // } 
-	   //      else {
-				// this->setCoreMode(PLANNING);
-				// this->sendGoal((*this).getGoal());
-				// this->setFollowingAgent(-1);
-	   //       }
-		}
+	while(ros::ok()){
 
 		ros::spinOnce();
 		r.sleep();
 	}
 }
 
-CoreMode NiceCore::getCoreMode(void){
-	CoreMode ret = IDLE;
-	{
-		boost::mutex::scoped_lock(cmMutex);
-		ret = coreMode;
-	}
-	return ret;
-}
-
-void NiceCore::setCoreMode(CoreMode c){
-	{
-		boost::mutex::scoped_lock(cmMutex);
-		coreMode = c;
-	}
-}
-
-
-geometry_msgs::PoseStamped NiceCore::getGoal(void){
-	boost::mutex::scoped_lock(goalLock);
-	geometry_msgs::PoseStamped ret = this->goal;
-	return ret;
-}
-
-void NiceCore::setGoal(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	boost::mutex::scoped_lock(goalLock);
-	this->goal = (*msg);
-}
-
-geometry_msgs::PoseStamped NiceCore::getRobotPosition(void){
-	boost::mutex::scoped_lock(feedbackLock);
-	geometry_msgs::PoseStamped ret = (*this).feedback.feedback.base_position;
-	return ret;
-}
-
-
-void NiceCore::sendGoal(const geometry_msgs::PoseStamped::ConstPtr& goal){
-	(*this).coreGoalPublisher.publish(*goal);
-}
-
-void NiceCore::sendGoal(const geometry_msgs::PoseStamped& goal){
-	(*this).coreGoalPublisher.publish(goal);
-}
-
-/*Callback functions*/
-void NiceCore::GoalListenerCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	/*Set the new goal*/
-	(*this).setGoal(msg);
-
-	CoreMode cmCopy = this->getCoreMode();
-
-	if(cmCopy == IDLE){
-		cmCopy = PLANNING;
-		this->setCoreMode(cmCopy);
-	}
-
-	if(cmCopy == PLANNING){
-		/*Send plan to move_base to start off the base*/
-		// (*this).sendGoal((*this).getGoal());
-	}
-
-}
-
-void NiceCore::robotPositionListenerCallBack(const move_base_msgs::MoveBaseActionFeedback::ConstPtr& msg){
-	boost::mutex::scoped_lock(feedbackLock);
-	this->feedback = (*msg);
-}
-
-void NiceCore::setFollowingAgent(int fa){
-	boost::mutex::scoped_lock(faLock);
-	followingAgent = fa;
-}
-
-int NiceCore::getFollowingAgent(void){
-	boost::mutex::scoped_lock(faLock);
-	int ret = this->followingAgent;
-	return ret;
-}
-
-
-double NiceCore::getDistanceSq(const geometry_msgs::Transform& transform){
-	double x = transform.translation.x;
-	double y = transform.translation.y;
-	double z = transform.translation.z;
-
-	double gx = goal.pose.position.x;
-	double gy = goal.pose.position.y;
-	double gz = goal.pose.position.z;
-
-	double dx = gx - x;
-	double dy = gy - y;
-	double dz = gz - z;
-
-	return dx*dx + dy*dy + dz*dz;
-}
-
-bool NiceCore::facingGoal(const geometry_msgs::Transform& transform){
-	double x = transform.translation.x;
-	double y = transform.translation.y;
-	double z = transform.translation.z;
-
-	double gx = goal.pose.position.x;
-	double gy = goal.pose.position.y;
-	double gz = goal.pose.position.z;
-
-	double dotProd = x*gx + y*gy + z*gz;
-
-	return dotProd >= 0.0;
-}
-
-void NiceCore::cancelGoal(const actionlib_msgs::GoalID goalId){
-	goalCanceller.publish(goalId);
-}
-
-
-void NiceCore::tfCallback(const tf::tfMessage::ConstPtr& msg){
-	
-	CoreMode cmCopy = this->getCoreMode();
-	if(cmCopy == PLANNING){
-		double minDistance = std::numeric_limits<double>::max();
-		int agentNo = -1;
-
-		for(unsigned i = 0; i < (*msg).transforms.size(); ++i){
-			tfparser::message m;
-			try{
-				m = tfMessageParser::parseFrame((std::string) (*msg).transforms.at(i).child_frame_id);
-			} catch(const FrameFormatNotCorrect &e){
-				continue;
-			}
-
-			if ((m.part == "/torso")){
-				geometry_msgs::Transform transform = (*msg).transforms.at(i).transform;
-				double agentDistance = this->getDistanceSq(transform);
-				bool facingGoal = this->facingGoal(transform);
-				if((facingGoal) && (minDistance < agentDistance)){
-					agentNo = m.user;
-					minDistance = agentDistance;
-				 }
-			}
-		}
-
-		if(agentNo != -1){
-			this->setCoreMode(FOLLOWING);
-			geometry_msgs::PoseStamped goalCopy = this->getGoal();
-			actionlib_msgs::GoalID goalId;
-			goalId.stamp = goalCopy.header.stamp;
-			goalId.id = goalCopy.header.frame_id;
-			this->cancelGoal(goalId);
-			this->setFollowingAgent(agentNo);
-		}
-
-	}
-}
-
-geometry_msgs::PointStamped NiceCore::getWorldPoint(geometry_msgs::PointStampedPtr& p, const char *frame){
-	geometry_msgs::PointStamped result;
-	try{
-		tfl.waitForTransform(p->header.frame_id, frame, p->header.stamp, ros::Duration(5.0));
-  		tfl.transformPoint(frame, *p, result);
-	}
-		catch (tf::TransformException &ex)
-	{
- 		ROS_ERROR("%s",ex.what());
-  		return geometry_msgs::PointStamped();
-	}
-	return result;
-}
-
-void NiceCore::detectionCallback(const nice_detector::DetectionListConstPtr& detections){
-	boost::mutex::scoped_lock(dqMutex);
-	for(unsigned i = 0; i < (*detections).detections.size(); ++i){
-
-		geometry_msgs::PointStampedPtr p (new geometry_msgs::PointStamped);
-		geometry_msgs::PointStampedPtr worldPoint (new geometry_msgs::PointStamped);
-		p->header.frame_id = detections->header.frame_id;
-		p->header.stamp = detections->header.stamp;				     
-		p->point.x = detections->detections[i].centroid.x;
-		p->point.y = detections->detections[i].centroid.y;
-		p->point.z = detections->detections[i].centroid.z;
-		
-		*worldPoint = getWorldPoint(p, "/odom");		
-		// *worldPoint = getWorldPoint(p, "/camera_depth_frame");		
-
-		DetectionMessage detectionMsg(
-			(*detections).detections.at(i).centroid,  
-			(*detections).detections.at(i).height,
-			(*detections).detections.at(i).confidence,
-			(*detections).detections.at(i).distance,
-			(*detections).detections.at(i).occluded,
-			(*detections).detections.at(i).header,
-			*worldPoint
-		);
-
-		this->detectionQueue.push(detectionMsg);
-	}
-}
-
-	
 }
